7bae28b3796717684ba63bf52b4008132aa6adb4
史晨亮 <1823793113@qq.com>
2020-11-16 23:47:05 +0800

    1.1

--- 抓罪犯/抓罪犯/test.cpp
+++ 抓罪犯/抓罪犯/test.cpp
@@ -11,87 +11,66 @@ typedef struct Suspect{
 	int isCriminal;
 }suspect;
 //打印
-void print(suspect a, suspect b, suspect c, suspect d){
+void print(suspect meb[4], int len){
 	printf("嫌疑人  话   罪犯\n");
-	printf("A       %d   %d\n", a.words, a.isCriminal);
-	printf("B       %d   %d\n", b.words, b.isCriminal);
-	printf("C       %d   %d\n", c.words, c.isCriminal);
-	printf("D       %d   %d\n", d.words, d.isCriminal);
+	char s = 'A';
+	for (int i = 0; i < len; i++){
+		printf("%c       %d   %d\n", s, meb[i].words, meb[i].isCriminal);
+		s++;
+	}
 }
 //初始化
-void Initialization(suspect* a, suspect* b, suspect* c, suspect* d){
-	a->words = true;
-	a->isCriminal = false;
-	b->words = true;
-	b->isCriminal = false;
-	c->words = true;
-	c->isCriminal = false;
-	d->words = true;
-	d->isCriminal = false;
-}
-void is_Criminal(suspect a, suspect b, suspect c, suspect d){
-
-	if (a.words == true)
-		a.isCriminal = false;
-	else a.isCriminal = true;
-	if (b.words == true)
-		c.isCriminal = true;
-	else c.isCriminal = false;
-	if (d.words == true)
-		c.words = false;
-	else c.words = true;
-	if (c.words == true)
-		d.isCriminal = true;
-	else d.isCriminal = false;
-	if (a.isCriminal == true && b.isCriminal == false && c.isCriminal == false && d.isCriminal == false){
-		printf("A是犯人.\n");
-		return;
+void Initialization(suspect meb[4],int len){
+	for (int i = 0; i < len; i++){
+		meb[i].words = true;
+		meb[i].isCriminal = false;
 	}
-
-
-	if (a.isCriminal == false && b.isCriminal == true && c.isCriminal == false && d.isCriminal == false){
-		printf("B是犯人.\n");
-		return;
-	}
-
-	if (a.isCriminal == false && b.isCriminal == false && c.isCriminal == true && d.isCriminal == false){
-		printf("C是犯人.\n");
-		return;
-	}
-
-	if (a.isCriminal == false && b.isCriminal == false && c.isCriminal == false && d.isCriminal == true){
-		printf("D是犯人.\n");
-		return;
-	}
-
-	else{
-		return;
-	} 
-	print(a, b, c, d);
 	
+	
+}
+void is_Criminal(suspect meb[4], int len){
+	for (int i = 0; i < len; i++){
+		meb[i].words = false;
+		if (meb[0].words == true)
+			meb[0].isCriminal = false;
+		else meb[0].isCriminal = true;
+		if (meb[1].words == true)
+			meb[2].isCriminal = true;
+		else meb[2].isCriminal = false;
+		if (meb[3].words == true)
+			meb[2].words = false;
+		else meb[2].words = true;
+		if (meb[2].words == true)
+			meb[3].isCriminal = true;
+		else meb[3].isCriminal = false;
+		if (meb[0].isCriminal == true && meb[1].isCriminal == false && meb[2].isCriminal == false && meb[3].isCriminal == false){
+			printf("A是犯人.\n");
+			return;
+		}
+		if (meb[0].isCriminal == false && meb[1].isCriminal == true && meb[2].isCriminal == false && meb[3].isCriminal == false){
+			printf("B是犯人.\n");
+			return;
+		}
+		if (meb[0].isCriminal == false && meb[1].isCriminal == false && meb[2].isCriminal == true && meb[3].isCriminal == false){
+			printf("C是犯人.\n");
+			return;
+		}
+		if (meb[0].isCriminal == false && meb[1].isCriminal == false && meb[2].isCriminal == false && meb[3].isCriminal == true){
+			printf("D是犯人.\n");
+			return;
+		}
+		meb[i].words = true;
+	}
+
 }
 int main(){
-	suspect a, b, c, d;
-	suspect* pa=&a;
-	suspect* pb=&b;
-	suspect* pc=&c;
-	suspect* pd=&d;
-	
+	suspect meb[4];
 
-	Initialization(pa, pb, pc, pd);
 
-	a.words = false;
-	is_Criminal(a, b, c, d);
-	Initialization(pa, pb, pc, pd);
-	b.words = false;
-	is_Criminal(a, b, c, d);
-	Initialization(pa, pb, pc, pd);
-	c.words = false;
-	is_Criminal(a, b, c, d);
-	Initialization(pa, pb, pc, pd);
-	d.words = false;
-	is_Criminal(a, b, c, d);
-	Initialization(pa, pb, pc, pd);
+	Initialization(meb,4);
+	print(meb, 4);
+	is_Criminal(meb, 4);
+
 	system("pause");
 	return 0;
 }
