952420be7e9eee5abfba0b882cf94f409e67fd5d
史晨亮 <1823793113@qq.com>
2020-12-01 23:27:31 +0800

    1.1

--- 求二叉树高度/求二叉树高度/test.cpp
+++ 求二叉树高度/求二叉树高度/test.cpp
@@ -1,52 +1,117 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+//ABD##FE###CG#H##I##
 
-typedef char ElementTyoe;
+typedef char ElementType;
 typedef struct TNode *Position;
 typedef Position BinTree;
 struct TNode{
-	ElementTyoe Date;
+	ElementType Data;
 	BinTree Left;
-	BinTree right;
+	BinTree Right;
 };
-BinTree CreatBinTree(BinTree &BT){
-	char x;
-	scanf("%c", &x);
-	if (x = '0'){
-		BT = NULL;
-		return 0;
-	}
 
-	BT = (BinTree)malloc(sizeof(struct TNode));
-	if (&BT == NULL)
-		return 0;
-	BT->Date = x;
-	CreatBinTree((BT->Left));
-	CreatBinTree((BT->right));
-	return BT;
-}
-void FreeBinTree(BinTree BT){
-	if (BT != NULL){
-		if (BT->Left){
-			FreeBinTree(BT->Left);
-			BT->Left = NULL;
-		}
-		if (BT->right){
-			FreeBinTree(BT->right);
-			BT->right = NULL;
+BinTree CreatBinTree(); //先序创建
+int GetHeight(BinTree BT);//树的高度
 
-		}
-	}
-}
+void InorderTraversal(BinTree BT);//中序遍历
+void PreorderTraversal(BinTree BT);//先序遍历
+void PostorderTraversal(BinTree BT);//后序遍历
+void Inorder_Traversal(BinTree BT);//中序遍历(非递归)
+void Preorder_Traversal(BinTree BT);//先序遍历(非递归)
+void Postorder_Traversal(BinTree BT);//后序遍历(非递归)
+void LevelorderTraversal(BinTree BT);//顺序遍历
+void PreorderPrintLeaves(BinTree BT);//先序输出叶结点
 
-int main(){
-	int arr[3][5] = { 0 };
-	BinTree BT;
-	CreatBinTree(BT);
-
+int main()
+{
+	BinTree BT = CreatBinTree();
+	printf("%d\n", GetHeight(BT));
+	printf("Inorder:");    InorderTraversal(BT);    printf("\n");
+	printf("Preorder:");   PreorderTraversal(BT);   printf("\n");
+	printf("Postorder:");  PostorderTraversal(BT);  printf("\n");
+	printf("Levelorder:"); LevelorderTraversal(BT); printf("\n");
+	printf("Leaf nodes are:");PreorderPrintLeaves(BT);printf("\n");
 	system("pause");
 	return 0;
+}
+
+//先序创建
+BinTree CreatBinTree(){
+	char ch;
+	scanf("%c", &ch);
+	if (ch == '#')return NULL;
+	else{
+		BinTree BT = (BinTree)malloc(sizeof(struct TNode));
+		BT->Data = ch;
+		BT->Left = CreatBinTree();
+		BT->Right = CreatBinTree();
+		return BT;
+	}
+}
+//树的高度
+int GetHeight(BinTree BT){
+	if (BT == NULL)
+		return 0;
+	else{
+		int l = GetHeight(BT->Left);
+		int r = GetHeight(BT->Right);
+		if (l > r)return(l + 1);
+		else return(r + 1);
+	}
+}
+//中序遍历
+void InorderTraversal(BinTree BT){
+	if (BT){
+		InorderTraversal(BT->Left);
+		printf("%2c", BT->Data);
+		InorderTraversal(BT->Right);
+	}
+}
+//先序遍历
+void PreorderTraversal(BinTree BT){
+	if (BT){
+		printf("%2c", BT->Data);
+		PreorderTraversal(BT->Left);
+		PreorderTraversal(BT->Right);
+	}
+}
+//后序遍历
+void PostorderTraversal(BinTree BT){
+	if (BT){
+		PostorderTraversal(BT->Left);
+		PostorderTraversal(BT->Right);
+		printf("%2c", BT->Data);
+	}
+}
+//层序遍历
+void LevelorderTraversal(BinTree BT){
+	BinTree temp[100];
+	int in = 0;
+	int out = 0;
+	
+	temp[in++] = BT;
+
+	while (in > out){
+		if (temp[out]){
+			printf("%2c", temp[out]->Data);
+			temp[in++] = temp[out]->Left;
+			temp[in++] = temp[out]->Right;
+		}
+		out++;
+	}
+}
+//先序输出叶结点
+void PreorderPrintLeaves(BinTree BT){
+	if (BT){
+		if ((BT->Left==NULL)&&(BT->Right==NULL))
+			printf("%2c", BT->Data);
+		PreorderPrintLeaves(BT->Left);
+		PreorderPrintLeaves(BT->Right);
+	}
+}
+//中序遍历(非递归)
+void Inorder_Traversal(BinTree BT){
 
 }
